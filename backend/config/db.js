c0aecbf8eda241e63a622f916c55993cfd383875
weihuan1007 <lolihuan@gmail.com>
2025-07-19 12:16:59 +0800

    change

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2');
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
-  port: process.env.PORT,   
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME
