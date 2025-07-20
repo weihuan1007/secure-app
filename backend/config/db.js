55dee083759fde939912ba817c73210561bb8031
weihuan1007 <lolihuan@gmail.com>
2025-07-20 14:57:09 +0800

    push

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,6 +2,7 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
+  port: process.env.DB_PORT || 3306,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME
 });
