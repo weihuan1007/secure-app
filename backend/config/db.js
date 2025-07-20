e5d9195933c0a4edbca148195e8666f01cd94a37
weihuan1007 <lolihuan@gmail.com>
2025-07-20 15:28:48 +0800

    revert

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,18 +1,12 @@
-const { Pool } = require('pg');
-require('dotenv').config();
-
-const pool = new Pool({
-  host: process.env.PGHOST,
-  port: process.env.PGPORT,
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-  database: process.env.PGDATABASE,
-  ssl: true // Required by Railway
+const mysql = require('mysql2');
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
-
-pool.connect((err) => {
+connection.connect((err) => {
   if (err) throw err;
-  console.log("PostgreSQL Connected!");
+  console.log('MySQL Connected!');
 });
-
-module.exports = pool;
+module.exports = connection;
